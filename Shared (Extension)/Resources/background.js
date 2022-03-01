d65ec66899f0111354d5f261838dddcd25813217
buchwasa <17461354+buchwasa@users.noreply.github.com>
2022-02-28 21:24:12 -0500

    Services are now downloaded to fetch a rating, added some additional logging

--- Shared (Extension)/Resources/background.js
+++ Shared (Extension)/Resources/background.js
@@ -3,6 +3,7 @@ function getDomainFromURL(urlString) {
     
     // I'm pretty sure Safari enforces https, but I do not know any http sites to confirm this.
     if (url.protocol !== "https:") {
+        console.log(hostname + " does not use a valid protocol");
         return null;
     }
     
@@ -14,12 +15,23 @@ function getDomainFromURL(urlString) {
     return hostname;
 }
 
-function downloadData() {
+//TODO: Store this locally, I don't understand why the API is this limited
+// that I have to go and download the entire database just to get what I need
+function getAllServices() {
+    console.log("Downloading service list");
+    let request = new Request("https://tosdr.org/api/v1/service/all.json");
     
+    return fetch(request).then(function(response) {
+        console.log("Downloaded list");
+        return response.json();
+    });
 }
 
 browser.tabs.onUpdated.addListener(function(id, info, tab) {
     if (info.status === "complete") {
-        
+        getAllServices().then(function(services) {
+            let service = services["tosdr/review/" + getDomainFromURL(tab.url)];
+            console.log(service);
+        });
     }
 });
